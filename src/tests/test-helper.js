a44592299da7a5bca3f9ddd202258be450fe4fd9
Ducthinh1501 <ducthinh150103@gmail.com>
2024-01-25 12:31:29 +0700

    Tai cau truc 1

---
+++ src/tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'project-8/app';
+import config from 'project-8/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
